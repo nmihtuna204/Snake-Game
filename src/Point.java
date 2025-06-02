b39b2c4f5ec47b9de6ca2d39dce488541f584207
nmihtuna204 <124556175+nmihtuna204@users.noreply.github.com>
2025-06-02 13:56:53 +0700

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,22 @@
 public class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point point = (Point) obj;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
 }
